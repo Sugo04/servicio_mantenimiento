6491acdee76f6eef0316438da0bab349a8792b8a
Héctor <hmarort075@g.educaand.es>
2025-02-10 22:42:53 +0100

    Editado archivo __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+'name': 'Servicio de Mantenimiento',
+'version': '1.0',
+'author': 'Héctor Martín Ortega',
+'category': 'Custom',
+'summary': 'Gestión de Mantenimiento de Productos',
+'depends': ['base', 'garantia.producto'],
+'data': [
+'security/ir.model.access.csv',
+'views/servicio_mantenimiento_views.xml',
+],
+'icon': '/garantias/static/description/icon55.png',
+'installable': True,
+'application': True,
+}
